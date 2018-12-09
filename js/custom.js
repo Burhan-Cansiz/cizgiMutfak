574ed315eb6a0ddf6b4304f71d13551aceff3dea
Burhan <burhancansiz@outlook.com>
2018-12-09 15:29:10 +0300

    .

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,8 @@
 $(function (){
-
+    $('.kategoriMenu h5').on('click',function (){
+        $('.kategoriMenu ul ul').stop().slideUp();
+        $(this).next('ul').stop().slideToggle();
+    });
   
   
 });
