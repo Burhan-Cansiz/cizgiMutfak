179525138d07c5203c13b11734b0f5c2997b3924
Burhan <burhancansiz@outlook.com>
2018-12-23 01:06:43 +0300

    ..
    .

--- js/custom.js
+++
@@ -1,8 +0,0 @@
-$(function (){
-    $('.kategoriMenu h6').on('click',function (){
-        $('.kategoriMenu ul ul').stop().slideUp();
-        $(this).next('ul').stop().slideToggle();
-    });
-  
-  
-});
